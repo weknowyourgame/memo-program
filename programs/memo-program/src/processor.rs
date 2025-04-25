ea6d6a2ea5e2f3c7a254da0ab8d99cecd9036f5b
Sarthak2 <aniki123.ai@gmail.com>
2025-04-25 15:15:19 +0530

    processor --done

--- programs/memo-program/src/processor.rs
+++ programs/memo-program/src/processor.rs
@@ -1,3 +1,4 @@
+// Program's processor 
 
 use {
     solana_account_info::{next_acount_info, AccountInfo}, solana_program_entrypoint::ProgramResult,
@@ -5,14 +6,24 @@ use {
 }
 
 pub fn process_instruction(
-    _program_id:[&Pubkey]
-    accounts:[&AccountInfo]
+    _program_id:[&Pubkey],
+    accounts:[&AccountInfo],
     input: &[u8]
 ) -> ProgramResult {
-    // the real deal build the memo and shii then sends it on the blockchain
-    let account = &mut accounts.iter()?;
+    let account = &mut accounts.iter();
     let account = next_account_info(account_info_iter)?;
 
+    let mut missing_required_signature = false;
+    for account_info in account_info_iter {
+        if let Some(address) = account_info.signer_key() {
+            msg!("Signed by {:?}", address);
+        } else {
+            missing_required_signature = true;
+        }
+    }
+    if missing_required_signature {
+        return Err(ProgramError::MessingRequiredSignature);    
+}
     
     let str = match from_utf8(input) {
     Ok(valid_str) => valid_str,
